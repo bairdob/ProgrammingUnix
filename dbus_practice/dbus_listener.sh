5c41a3fa52f92c762588351173248f980c709281
bairdob <krumane@mail.ru>
2023-03-23 23:02:32 +0300

    remove comment

--- dbus_practice/dbus_listener.sh
+++ dbus_practice/dbus_listener.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-#dbus-monitor --system "sender=org.freedesktop.NetworkManager, path=/org/freedesktop/NetworkManager, member=StateChanged" | sed -u -n 's/   uint32 20/python3 \/home\/bair\/kernel_practice\/dbus\/network_stop.py/p' | sh
+
 dbus-monitor --system "sender=org.freedesktop.NetworkManager, path=/org/freedesktop/NetworkManager, member=StateChanged" | sed -u -n 's/   uint32 20/\/usr\/bin\/python3 \/home\/bair\/kernel_practice\/dbus\/network_stop.py/p; s/   uint32 70/python3 \/home\/bair\/kernel_practice\/dbus\/network_start.py/p' | sh
