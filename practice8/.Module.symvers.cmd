5b94ad6d317522adf6d4ab32dac4134be561045b
bairdob <krumane@mail.ru>
2023-04-02 22:30:56 +0300

    remove dotfiles

--- practice8/.Module.symvers.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/bair/kernel_practice/practice8/Module.symvers := sed 's/ko$$/o/' /home/bair/kernel_practice/practice8/modules.order | scripts/mod/modpost -m -a  -o /home/bair/kernel_practice/practice8/Module.symvers -e -i Module.symvers   -T -
