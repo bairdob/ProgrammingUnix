54af4df012836d5af338a421278686b8bacdd20a
bairdob <krumane@mail.ru>
2023-04-02 22:31:35 +0300

    remove dotfiles

--- practice8/.modules.order.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/bair/kernel_practice/practice8/modules.order := {   echo /home/bair/kernel_practice/practice8/pci.ko; :; } | awk '!x[$$0]++' - > /home/bair/kernel_practice/practice8/modules.order
