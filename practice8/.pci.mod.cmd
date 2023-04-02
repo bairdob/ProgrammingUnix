52f36c01174c2906257d6b487d76e2bef04d9c42
bairdob <krumane@mail.ru>
2023-04-02 22:30:12 +0300

    remove dotfiles

--- practice8/.pci.mod.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/bair/kernel_practice/practice8/pci.mod := printf '%s\n'   pci.o | awk '!x[$$0]++ { print("/home/bair/kernel_practice/practice8/"$$0) }' > /home/bair/kernel_practice/practice8/pci.mod
