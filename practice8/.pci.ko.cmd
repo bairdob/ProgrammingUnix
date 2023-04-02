12baf13fcbb798eed53e7827e2f8f004f10182a0
bairdob <krumane@mail.ru>
2023-04-02 22:30:12 +0300

    remove dotfiles
    (cherry picked from commit 54af4df012836d5af338a421278686b8bacdd20a)
    
    remove dotfiles
    
    (cherry picked from commit 5b94ad6d317522adf6d4ab32dac4134be561045b)
    
    remove dotfiles
    
    (cherry picked from commit 52f36c01174c2906257d6b487d76e2bef04d9c42)

--- practice8/.pci.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/bair/kernel_practice/practice8/pci.ko := ld -r -m elf_x86_64 -z noexecstack --build-id=sha1  -T scripts/module.lds -o /home/bair/kernel_practice/practice8/pci.ko /home/bair/kernel_practice/practice8/pci.o /home/bair/kernel_practice/practice8/pci.mod.o;  true
