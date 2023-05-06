71e01901907679576105ad5b974d29004d2cb227
bairdob <krumane@mail.ru>
2023-05-06 22:11:04 +0300

    refactor

--- practice8/pci_skel.c
+++ practice8/pci_skel.c
@@ -26,19 +26,24 @@ static long my_ioctl(struct file *filep, unsigned int cmd, unsigned long arg);
 unsigned char mac_addr[ETH_ALEN];
 static char *mac_Ptr; 
 static int Device_Open = 0;	
-static int Major; /* Major-номер, для нашего устройства */
+static int Major; 
 
+void __iomem *mmio_addr;
+u8 *mac_ptr;
 
 static struct pci_device_id ids[] = {
 	{ PCI_DEVICE(PCI_VENDOR_ID, PCI_DEVICE_ID) },
 	{ 0, }
 };
 
+static struct file_operations fops = {
+	.read = device_read,
+	.unlocked_ioctl = my_ioctl,
+	.open = device_open,
+	.release = device_release
+};
 
 int get_mac_address(struct pci_dev *pdev, unsigned char *mac_addr) {
-    void __iomem *mmio_addr;
-    u8 *mac_ptr;
-
     // Map the PCI device memory into kernel space
     mmio_addr = ioremap(pci_resource_start(pdev, 0), pci_resource_len(pdev, 0));
     if (!mmio_addr) {
@@ -49,23 +54,9 @@ int get_mac_address(struct pci_dev *pdev, unsigned char *mac_addr) {
     mac_ptr = (u8 *)(mmio_addr + 0x0000);
     memcpy(mac_addr, mac_ptr, ETH_ALEN);
 
-    // Unmap the memory region
-    iounmap(mmio_addr);
-
     return 0;
 }
 
-/* kernel builds a table during build time in
-   /lib/modules/<kern>modules.pcimap that which
-   driver is supported for particular pci_vendor
- */
-// MODULE_DEVICE_TABLE(pci,pci_tbl);
-
-/*  1. 	Probe is called if a matching device in pci_tbl is found in the
- *  	the  System ,Called once for each matching device .if already called
- *  	another module , probe will not be called.
- *  2. 	when in Sysfs bind and new_id macthcing id found then Also probe is called
- */
 static int my_probe(struct pci_dev *dev, const struct pci_device_id *id)
 {
 	
@@ -78,7 +69,17 @@ static int my_probe(struct pci_dev *dev, const struct pci_device_id *id)
 	}
 
 	printk(KERN_INFO "MAC address: %pM\n", mac_addr);
-	
+
+	Major = register_chrdev(0, DEVICE_NAME, &fops);
+	if (Major < 0) { 
+		printk(KERN_ALERT "Регистрация симв. устройства failed для %d\n", Major); 	
+		return Major; 
+	}
+	printk(KERN_INFO "To talk to the driver, create a dev file with\n"); 
+	printk(KERN_INFO "'mknod /dev/%s c %d 0'.\n", DEVICE_NAME, Major); 
+	printk(KERN_INFO "Try various minor numbers. Try to cat and echo to\n"); 
+	printk(KERN_INFO "the device file.\n"); 
+
 	return 0;
 }
 
@@ -95,27 +96,11 @@ static struct pci_driver pci_driver = {
 	.remove = my_remove,
 };
 
-static struct file_operations fops = {
-	.read = device_read,
-	.unlocked_ioctl = my_ioctl,
-	.open = device_open,
-	.release = device_release
-};
 
 /*called while loading driver*/
 int init_module(void)
 {	
 	printk(KERN_INFO "*** inside init  *** \n");
-
-	Major = register_chrdev(0, DEVICE_NAME, &fops);
-	if (Major < 0) { 
-		printk(KERN_ALERT "Регистрация симв. устройства failed для %d\n", Major); 	
-		return Major; 
-	}
-	printk(KERN_INFO "To talk to the driver, create a dev file with\n"); 
-	printk(KERN_INFO "'mknod /dev/%s c %d 0'.\n", DEVICE_NAME, Major); 
-	printk(KERN_INFO "Try various minor numbers. Try to cat and echo to\n"); 
-	printk(KERN_INFO "the device file.\n"); 
 	
 	/*register the pci_driver structure with pci subsystem*/
 	return pci_register_driver(&pci_driver);
@@ -129,10 +114,7 @@ void cleanup_module(void)
 }
 
 
-static ssize_t device_read(struct file *filp, /* смотри include/linux/fs.h */
-			   char *buffer, /* буфер для данных */ 
-			   size_t length, /* длина буфера */ 
-			   loff_t * offset) /* смещение */ {
+static ssize_t device_read(struct file *filp, char *buffer, size_t length, loff_t * offset) {
 	size_t bytes_to_copy = min(length, ETH_ALEN - *offset);
 	if (bytes_to_copy <= 0){
 		return 0;
@@ -157,8 +139,11 @@ static int device_open(struct inode *inode, struct file *file) {
 }
 
 static int device_release(struct inode *inode, struct file *file) { 
-	Device_Open--; /* Мы теперь готовы для следующего вызова */
+	Device_Open--;
 	module_put(THIS_MODULE);
+
+	// Unmap the memory region
+    iounmap(mmio_addr);
 
 	return 0;
 }
